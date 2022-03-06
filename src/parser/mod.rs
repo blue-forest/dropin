b6a92109a70fb328142c979ff43d4ce483dc2917
Nazim Lachter <nazim@blueforest.cc>
2022-03-06 21:07:38 +0100

    sed fix

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,3 +1,11 @@
+/*     _              _ _
+ *  __| |_ _ ___ _ __( |_)_ _
+ * / _` | '_/ _ \ '_ \/| | ' \
+ * \__,_|_| \___/ .__/ |_|_||_| drop'in © 2019-2022 Blue Forest
+ *              |_|
+ * This code is free software distributed under GPLv3.
+ */
+
 mod types;
 pub use types::read_type;
 

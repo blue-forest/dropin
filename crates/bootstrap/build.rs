8942c29b0ae563143f2319ef3f6b0bd610de069a
vulcain <vulcain@blueforest.cc>
2022-04-25 14:24:07 +0200

    copy dropin_modules to out dir

---
+++ crates/bootstrap/build.rs
@@ -0,0 +1,19 @@
+use std::env::var;
+use std::fs::copy;
+use std::path::Path;
+
+fn main() {
+  let workspace_dir = var("CARGO_MANIFEST_DIR").unwrap();
+  let dropin_modules_path = Path::new(&workspace_dir).join(Path::new(
+    "../../target/wasm32-unknown-unknown/release/dropin_modules.wasm",
+  ));
+  if !dropin_modules_path.exists() {
+    panic!(
+      "Please compile drop\'in modules:\n\
+      cargo b -p dropin-modules --target wasm32-unknown-unknown --release"
+    );
+  }
+  let out_dir = var("OUT_DIR").unwrap();
+  let out_path = Path::new(&out_dir).join("dropin_modules.wasm");
+  copy(dropin_modules_path, out_path).unwrap();
+}

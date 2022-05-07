953d0ab88f934978ad0446bbac8b00e660ea1cb8
vulcain <vulcain@blueforest.cc>
2022-05-07 10:15:44 +0200

    fix licence header again

--- crates/bootstrap/src/sys/rights.rs
+++ crates/bootstrap/src/sys/rights.rs
@@ -1,3 +1,24 @@
+/*     _              _ _
+ *  __| |_ _ ___ _ __( |_)_ _
+ * / _` | '_/ _ \ '_ \/| | ' \
+ * \__,_|_| \___/ .__/ |_|_||_| dropin-compiler - WebAssembly
+ *              |_|
+ * Copyright © 2019-2022 Blue Forest
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 pub const FD_DATASYNC:             u64 = 1 <<  0;
 pub const FD_READ:                 u64 = 1 <<  1;
 pub const FD_SEEK:                 u64 = 1 <<  2;

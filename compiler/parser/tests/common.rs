3340e0c977117daf45da67b268aa2f2a05322a9f
Nazim Lachter <nazim@blueforest.cc>
2024-03-01 17:50:48 +0100

    lexer tests

---
+++ compiler/parser/tests/common.rs
@@ -0,0 +1,33 @@
+/*     _              _ _
+ *  __| |_ _ ___ _ __( |_)_ _
+ * / _` | '_/ _ \ '_ \/| | ' \
+ * \__,_|_| \___/ .__/ |_|_||_| dropin-compiler
+ *              |_|
+ * Copyright © 2019-2024 Blue Forest
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+use dropin_parser::{lexer::lexer, token::Token};
+
+pub fn test_lexer(input: &str, expected: Vec<Token>) {
+	println!("> `{}`", input);
+	let result = lexer(input);
+	if result != expected {
+		eprintln!(
+			"    /!\\ Test failed /!\\\n    Expected: {:?}\n    Got:      {:?}\n",
+			expected, result,
+		);
+	}
+}

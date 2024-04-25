3b04d4e0369281e8dd59360d721d4cd054468c6b
Nazim Lachter <nazim@blueforest.cc>
2024-04-25 16:32:17 +0200

    license header

--- compiler/parser/src/parser/stack.rs
+++ compiler/parser/src/parser/stack.rs
@@ -1,3 +1,24 @@
+/*     _              _ _
+ *  __| |_ _ ___ _ __( |_)_ _
+ * / _` | '_/ _ \ '_ \/| | ' \
+ * \__,_|_| \___/ .__/ |_|_||_| dropin-compiler - WebAssembly
+ *              |_|
+ * Copyright © 2019-2024 Blue Forest
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 use std::fmt::{Debug, Formatter};
 
 use dropin_compiler_common::token::TokenKind;

fef86d9614cfa7885c769855366c39ad17b40da6
vulcain <vulcain@blueforest.cc>
2024-06-04 11:37:55 +0200

    comparisons

--- compiler/targets/flutter/src/gen/expressions.rs
+++ compiler/targets/flutter/src/gen/expressions.rs
@@ -1,10 +1,11 @@
 use alloc::{fmt, string::String};
 use dropin_compiler_recipes::ir::{Expression, ExpressionInner};
 
-use self::value::gen_value;
+use self::{comparison::gen_comparison, value::gen_value};
 
 use super::Sub;
 
+mod comparison;
 mod value;
 
 pub fn gen_expressions<'a, S>(
@@ -18,7 +19,9 @@ where
 {
   match expression.expression_inner.as_ref().unwrap() {
     ExpressionInner::Value(value) => gen_value(output, state, trace, value)?,
-    ExpressionInner::Comparison(_) => todo!(),
+    ExpressionInner::Comparison(comparison) => {
+      gen_comparison(output, state, comparison)?
+    }
     ExpressionInner::Logic(_) => todo!(),
     ExpressionInner::Control(value) => todo!("{value:?}"),
     ExpressionInner::Arithmetic(_) => todo!(),
---
+++ compiler/targets/flutter/src/gen/expressions/comparison.rs
@@ -0,0 +1,56 @@
+use alloc::{
+  fmt::{self, Write},
+  string::String,
+};
+use dropin_compiler_recipes::ir::{Comparison, ComparisonInner};
+
+use crate::gen::{expressions::gen_expressions, Sub};
+
+pub fn gen_comparison<'a, S>(
+  output: &mut String,
+  state: &S,
+  comparison: &Comparison,
+) -> fmt::Result
+where
+  S: Sub<'a>,
+{
+  match comparison.comparison_inner.as_ref().unwrap() {
+    ComparisonInner::EqualsTo(comparison) => {
+      gen_expressions(output, state, &[], comparison.left.as_ref().unwrap())?;
+      write!(output, "==")?;
+      gen_expressions(output, state, &[], comparison.right.as_ref().unwrap())?;
+    }
+    ComparisonInner::DifferentFrom(comparison) => {
+      gen_expressions(output, state, &[], comparison.left.as_ref().unwrap())?;
+      write!(output, "!=")?;
+      gen_expressions(output, state, &[], comparison.right.as_ref().unwrap())?;
+    }
+    ComparisonInner::In(comparison) => {
+      gen_expressions(output, state, &[], comparison.left.as_ref().unwrap())?;
+      write!(output, ".contains(")?;
+      gen_expressions(output, state, &[], comparison.right.as_ref().unwrap())?;
+      write!(output, ")")?;
+    }
+    ComparisonInner::LessThan(comparison) => {
+      gen_expressions(output, state, &[], comparison.left.as_ref().unwrap())?;
+      write!(output, "<")?;
+      gen_expressions(output, state, &[], comparison.right.as_ref().unwrap())?;
+    }
+    ComparisonInner::MoreThan(comparison) => {
+      gen_expressions(output, state, &[], comparison.left.as_ref().unwrap())?;
+      write!(output, ">")?;
+      gen_expressions(output, state, &[], comparison.right.as_ref().unwrap())?;
+    }
+    ComparisonInner::AtLeast(comparison) => {
+      gen_expressions(output, state, &[], comparison.left.as_ref().unwrap())?;
+      write!(output, ">=")?;
+      gen_expressions(output, state, &[], comparison.right.as_ref().unwrap())?;
+    }
+    ComparisonInner::AtMost(comparison) => {
+      gen_expressions(output, state, &[], comparison.left.as_ref().unwrap())?;
+      write!(output, "<=")?;
+      gen_expressions(output, state, &[], comparison.right.as_ref().unwrap())?;
+    }
+  }
+  todo!()
+}

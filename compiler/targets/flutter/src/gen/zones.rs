2e85290394be8c7fff56407fd525b949d97d2737
vulcain <vulcain@blueforest.cc>
2024-06-18 15:19:48 +0200

    listenablebuilders

--- compiler/targets/flutter/src/gen/zones.rs
+++ compiler/targets/flutter/src/gen/zones.rs
@@ -1,11 +1,16 @@
 use alloc::{
   fmt::{self, Write},
   string::String,
+  vec::Vec,
 };
 use dropin_compiler_common::to_upper_camelcase;
 use dropin_compiler_recipes::ir::{ComponentChildInner, ComponentZone};
 
-use crate::gen::expressions::gen_rich_text;
+use crate::{
+  gen::expressions::gen_rich_text,
+  setters_listeners::{write_notifier_name, SettersAndListenersState},
+  Stated,
+};
 
 use super::{expressions::gen_getter, Sub};
 
@@ -13,15 +18,48 @@ pub fn gen_zone<'a, S>(
   output: &mut String,
   component: &str,
   state: &S,
-  _trace: &[usize],
+  trace: &[usize],
   zone: &ComponentZone,
 ) -> fmt::Result
 where
   S: Sub<'a>,
 {
   write!(output, "Row(children: [")?;
+  let setters_listeners = <S as Stated<SettersAndListenersState>>::state(state);
+  let updated_getters = setters_listeners.get_updated_getters(component);
   for (i, child) in zone.blocks.iter().enumerate() {
-    // let trace = &[trace, &[i]].concat();
+    let trace = &[trace, &[i]].concat();
+    let updated_listeners = setters_listeners
+      .get_listeners(component, trace)
+      .map(|listeners| {
+        listeners
+          .iter()
+          .filter(|listener| {
+            updated_getters
+              .iter()
+              .position(|updated| *updated == listener.getter)
+              .is_some()
+          })
+          .collect::<Vec<_>>()
+      });
+    let mut is_listenable = false;
+    if let Some(updated_listeners) = updated_listeners {
+      if !updated_listeners.is_empty() {
+        assert_eq!(updated_listeners.len(), 1, "TODO: add Listenable.merge()");
+        is_listenable = true;
+        let listener = &updated_listeners[0];
+        write!(
+          output,
+          "ListenableBuilder(\
+          listenable:",
+        )?;
+        write_notifier_name(output, listener.getter)?;
+        write!(
+          output,
+          ", builder: (BuildContext context, Widget? child) => "
+        )?;
+      }
+    }
     if i != 0 {
       write!(output, ",")?;
     }
@@ -60,6 +98,9 @@ where
       ComponentChildInner::Extern(r#extern) => {
         write!(output, "{}()", to_upper_camelcase(&r#extern.path))?;
       }
+    }
+    if is_listenable {
+      write!(output, ")")?;
     }
   }
   write!(output, "])")?;
--- compiler/targets/flutter/src/setters_listeners.rs
+++ compiler/targets/flutter/src/setters_listeners.rs
@@ -2,22 +2,60 @@ use crate::properties_resolver::PropertiesResolverState;
 use crate::visit::{ComponentChildTrace, ExpressionTrace, Visit};
 use alloc::borrow::Cow;
 use alloc::collections::BTreeMap;
+use alloc::fmt::{self, Write};
+use alloc::string::String;
 use alloc::vec::Vec;
-use dropin_compiler_recipes::ir::{Component, ComponentInput, Getter};
+use dropin_compiler_common::to_upper_camelcase;
+use dropin_compiler_recipes::ir::{
+  Component, ComponentInput, ExpressionInner, Getter, RichText, RichTextInner,
+  RichTextPart, Value, ValueInner,
+};
 
 #[derive(Debug)]
 pub struct SettersAndListenersState<'a> {
-  setters: BTreeMap<&'a str, Vec<Cow<'a, Getter>>>,
-  listeners:
-    BTreeMap<&'a str, BTreeMap<Vec<usize>, Vec<(&'a str, Cow<'a, Getter>)>>>,
+  setters: BTreeMap<&'a str, Vec<&'a Getter>>,
+  listeners: BTreeMap<&'a str, BTreeMap<Vec<usize>, Vec<Listener<'a>>>>,
+}
+
+#[derive(Debug)]
+pub struct Listener<'a> {
+  pub getter: &'a Getter,
+  pub resolved: Vec<Resolved<'a>>,
+}
+
+#[derive(Debug)]
+pub struct Resolved<'a> {
+  pub owner: &'a str,
+  pub getter: Cow<'a, Getter>,
+}
+
+impl<'a> SettersAndListenersState<'a> {
+  pub fn get_listeners(
+    &self,
+    component: &str,
+    trace: &[usize],
+  ) -> Option<&[Listener<'a>]> {
+    self
+      .listeners
+      .get(component)
+      .and_then(|listeners| listeners.get(trace))
+      .map(|listeners| listeners.as_slice())
+  }
+
+  pub fn get_updated_getters(&self, component: &str) -> &[&'a Getter] {
+    self
+      .setters
+      .get(component)
+      .map(|getters| getters.as_slice())
+      .unwrap_or(&[])
+  }
 }
 
 pub struct SettersAndListeners<'a, 'b> {
   resolver: &'b PropertiesResolverState<'a>,
   component: Option<&'a str>,
-  setters: BTreeMap<&'a str, Vec<Cow<'a, Getter>>>,
-  listeners:
-    BTreeMap<&'a str, BTreeMap<Vec<usize>, Vec<(&'a str, Cow<'a, Getter>)>>>,
+  setters: BTreeMap<&'a str, Vec<&'a Getter>>,
+  listeners: BTreeMap<&'a str, BTreeMap<Vec<usize>, Vec<Listener<'a>>>>,
 }
 
 impl<'a, 'b> SettersAndListeners<'a, 'b> {
@@ -52,23 +90,11 @@ impl<'a, 'b> Visit<'a, SettersAndListenersState<'a>>
   ) {
     let getter = input.on_change.as_ref().unwrap();
     let component = self.component.unwrap();
-    let resolved = if let Some(resolved) = self
-      .resolver
-      .0
-      .get(component)
-      .and_then(|resolved| resolved.get(getter.ident.as_str()))
-    {
-      resolved
-    } else {
-      &BTreeMap::from([(component, Vec::from([Cow::Borrowed(getter)]))])
-    };
-    for (owner, getters) in resolved {
-      self
-        .setters
-        .entry(owner)
-        .or_insert(Vec::with_capacity(getters.len()))
-        .extend(getters.clone());
-    }
+    self
+      .setters
+      .entry(component)
+      .or_insert(Vec::with_capacity(1))
+      .push(getter);
   }
 
   fn visit_getter(
@@ -101,14 +127,57 @@ impl<'a, 'b> Visit<'a, SettersAndListenersState<'a>>
     } else {
       &BTreeMap::from([(component, Vec::from([Cow::Borrowed(getter)]))])
     };
+    let mut listener = Listener {
+      getter,
+      resolved: Vec::with_capacity(resolved.len()),
+    };
     for (owner, getters) in resolved {
-      self
-        .listeners
-        .entry(component)
-        .or_insert(BTreeMap::new())
-        .entry(trace.indexes.clone())
-        .or_insert(Vec::with_capacity(getters.len()))
-        .extend(getters.clone().into_iter().map(|getter| (*owner, getter)));
+      listener.resolved.extend(
+        getters
+          .clone()
+          .into_iter()
+          .map(|getter| Resolved { owner, getter }),
+      );
+    }
+    self
+      .listeners
+      .entry(component)
+      .or_insert(BTreeMap::new())
+      .entry(trace.indexes.clone())
+      .or_insert(Vec::with_capacity(1))
+      .push(listener)
+  }
+}
+
+pub fn write_notifier_name(
+  output: &mut String,
+  getter: &Getter,
+) -> fmt::Result {
+  write!(output, "notifier{}", to_upper_camelcase(&getter.ident))?;
+  for key in &getter.indexes {
+    let ExpressionInner::Value(Value {
+      value_inner: Some(value_inner),
+    }) = key.expression_inner.as_ref().unwrap()
+    else {
+      break;
+    };
+    match value_inner {
+      ValueInner::Text(RichText { parts }) => {
+        if parts.len() == 1 {
+          if let RichTextPart {
+            rich_text_inner: Some(RichTextInner::Static(part)),
+          } = &parts[0]
+          {
+            write!(output, "{}", to_upper_camelcase(part))?;
+          }
+        }
+      }
+      ValueInner::Quantity(index) => {
+        write!(output, "{index}")?;
+      }
+      _ => break,
     }
   }
+  write!(output, "_")?;
+  Ok(())
 }
